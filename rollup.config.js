900fcbefd09c65d7fd3f1b16a553f5b170aa88fc
YASHMAHAKAL <yvsst01@gmail.com>
2026-02-17 16:50:45 +0530

    initial migration
    Signed-off-by: YASHMAHAKAL <yvsst01@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import babel from '@rollup/plugin-babel';
+import { swc } from 'rollup-plugin-swc3';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
 import uglify from '@lopatnov/rollup-plugin-uglify';
@@ -13,9 +13,37 @@ export default {
     commonjs({
       include: ['node_modules/**'],
     }),
-    babel({
-      babelHelpers: 'runtime',
-      babelrc: true,
+    swc({
+      include: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      tsconfig: false,
+      jsc: {
+        parser: {
+          syntax: 'ecmascript',
+          jsx: true,
+        },
+        transform: {
+          react: {
+            runtime: 'automatic',
+          },
+        },
+        externalHelpers: false,
+      },
+      env: {
+        targets: {
+          ie: '11',
+          chrome: '58',
+          firefox: '54',
+          safari: '10',
+          edge: '15',
+        },
+        mode: 'usage',
+        coreJs: '3',
+      },
+      module: {
+        type: 'es6',
+      },
+      sourceMaps: true,
     }),
     uglify({
       compress: {
